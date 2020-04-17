e25e2255801be3fb37ef17fbe9bb69960250a94b
Sergey <isb9797@gmail.com>
2020-04-17 16:41:45 +0300

    Lesson 13 complete

--- js/common.js
+++ js/common.js
@@ -1,107 +1,77 @@
-"use strict";
+'use strict';
+//Урок 13 TODO 
 
-const todoControl = document.querySelector(".todo-control"),
-  headerInput = document.querySelector(".header-input"),
-  todoList = document.querySelector(".todo-list"),
-  todocompleted = document.querySelector(".todo-completed");
 
-let todoData = [];
+//Получаем основные элементы
+const todoControl = document.querySelector('.todo-control'),
+  headerInput = document.querySelector('.header-input'),
+  todoList = document.querySelector('.todo-list'),
+  todoCompleted = document.querySelector('.todo-completed');
 
-const render = function () {
-  todoList.textContent = "";
-  todocompleted.textContent = "";
-  todoData = JSON.parse(localStorage.getItem("todoData"));
-  if (todoData === null) {
+//Получаем JSON из localStorage
+let todoData = JSON.parse(localStorage.getItem('todoData'));
+
+//Чтобы небыло ошибки присваиваем todoData пустой массив
+if (todoData === null){
     todoData = [];
-  } else {
-    todoData.forEach(function (item) {
-      const li = document.createElement("li");
-      li.classList.add("todo-item");
-      li.innerHTML =
-        '<span class="text-todo">' +
-        item.value +
-        "</span>" +
+}
+
+const render = () => {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    
+    //Долго и нудно работаем с данными
+    todoData.forEach((item) => {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
         '<div class="todo-buttons">' +
-        '<button class="todo-remove"></button>' +
-        '<button class="todo-complete"></button>' +
-        "</div>";
+            '<button class="todo-remove"></button>' + 
+            '<button class="todo-complete"></button>' +
+        '</div>';
+        
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
 
-      if (item.completed) {
-        todocompleted.append(li);
-      } else {
-        todoList.append(li);
-      }
+        //
+        const btnComplete = li.querySelector('.todo-complete');
+        btnComplete.addEventListener('click', () =>{
+            item.completed = !item.completed;
+            render();
+        });
 
-      const todoCompletedBtn = li.querySelector(".todo-complete");
-      todoCompletedBtn.addEventListener("click", function () {
-        item.completed = !item.completed;
-
-        let elVal = this.parentElement.parentElement.textContent;
-
-        let rewriteArray = JSON.parse(localStorage.getItem("todoData"));
-
-        for (let i of rewriteArray) {
-          for (let k in rewriteArray) {
-            if (i.value === elVal && i.complete === false) {
-              rewriteArray.splice(k - 1, 1, {
-                value: i.value,
-                complete: true,
-              });
-            } else if (i.value === elVal && i.complete === false) {
-              //todocompleted.append(li);
-              rewriteArray.splice(k - 1, 1, {
-                value: i.value,
-                complete: true,
-              });
+        const btnRemove = li.querySelector('.todo-remove');
+       btnRemove.addEventListener('click', () => {
+            let newTodo = [];
+            for (let index = 0; index <todoData.length; index++) {
+                 if (item.value !== todoData[index].value){
+                    newTodo.push(todoData[index]);
+                 }
             }
-          }
-        }
-        console.log("rewriteArray: ", rewriteArray);
-      });
-
-      const todoRemoveBtn = li.querySelector(".todo-remove");
-      todoRemoveBtn.addEventListener("click", function () {
-        let elVal = this.parentElement.parentElement.textContent;
-
-        let removeArray = JSON.parse(localStorage.getItem("todoData"));
-
-        for (let k of removeArray) {
-          for (let i in removeArray) {
-            if (k.value === elVal) {
-              //console.log('removeArray: ', removeArray[i]);
-              removeArray.splice(i, 1);
-              console.log("removeArray: ", removeArray);
-            }
-          }
-        }
-        localStorage.removeItem("todoData");
-        if (removeArray.length !== 0) {
-          localStorage.setItem("todoData", JSON.stringify(removeArray));
-        }
-
-        //todoData = JSON.parse(localStorage.getItem('todoData'));
-        render();
-      });
+            todoData = newTodo;
+            render();
+        });
     });
-  }
+    headerInput.value ='';
+    localStorage.setItem('todoData', JSON.stringify(todoData));
 };
 
-todoControl.addEventListener("submit", function (event) {
-  event.preventDefault();
+todoControl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (headerInput.value !== ''){
+        const newTodo = {
+            value: headerInput.value,
+            completed: false
+        };
+    
+        todoData.push(newTodo);
 
-  if (headerInput.value === "") {
-    return;
-  } else {
-    const newTodo = {
-      value: headerInput.value,
-      complete: false,
-    };
-    todoData.push(newTodo);
-    //console.log('todoData: ', todoData);
-    localStorage.setItem("todoData", JSON.stringify(todoData));
-    headerInput.value = "";
-    render();
-  }
+        render();
+        
+    }
 });
 
 render();
